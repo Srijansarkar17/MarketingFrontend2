7b83fa989b34fabc0f42d5968c66e858e02e3ab9
Srijan Sarkar <srijanb463@gmail.com>
2025-12-24 12:06:44 +0530

    supabase version changed

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-console.log('VITE ROOT:', process.cwd());
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  optimizeDeps: {
+    include: ['@supabase/supabase-js'],
+  },
+  ssr: {
+    noExternal: ['@supabase/supabase-js'],
+  },
   resolve: {
     alias: {
       '@': '/src',
